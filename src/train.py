8272b842b9fe6c9785cfffb5a5b6690078a9a6da
mohamedezzat195 <65695739+mohamedezzat195@users.noreply.github.com>
2026-01-05 14:22:50 +0200

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,14 @@
+import yfinance as yf
+import pandas as pd
 
+def download_data():
+    print("Downloading financial data from Yahoo Finance...")
+    #  S&P 500
+    df = yf.download('^GSPC', start='2015-01-01', end='2025-12-31')
+    
+    
+    df[['Close']].to_csv('data/financial_data.csv')
+    print("Data saved to data/financial_data.csv")
+
+if __name__ == "__main__":
+    download_data()
